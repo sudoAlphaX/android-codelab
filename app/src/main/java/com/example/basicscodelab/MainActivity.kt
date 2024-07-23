bd8c3ad6af74a5cffcf8b7d0646ffb168a9c2eb6
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-23 23:30:09 +0530

    fix extra braces syntax error

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicsCodelabTheme {
                 MyApp(modifier = Modifier.fillMaxSize())
-                }
             }
         }
     }
